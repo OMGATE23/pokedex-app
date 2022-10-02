6ffb6a165c44bc5eb7b53d79b562b920915de202
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-03 00:32:48 +0530

    feat: added pagination

---
+++ src/Pagination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pagination({gotoNextPage, gotoPrevPage}) {
+  return (
+    <div>
+        {gotoNextPage && <button onClick={gotoNextPage}>Next Page</button>}
+        {gotoPrevPage && <button onClick={gotoPrevPage}>Previous Page</button>}
+
+    </div>
+  )
+}
